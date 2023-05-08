e4dc8fe964aa13d03422e3fe16bde966fcf91cc5
Thakur Aditya Dhanraj Singh <aditya.dhanraj@internal.utradesolutions.com>
2023-05-08 23:12:06 +0530

    Moving to Main branch, saving basic work here

--- hw/main.go
+++ hw/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -24,10 +25,32 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	a := 100.0
+	b := 0.0
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", a, b, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by 0")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // the main application function.
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting Application at %s", portNumber))
 
